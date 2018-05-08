e24174ee807d9afb4a73da310b68d59ecc6642ac
Joe Maller <joe@ideasonpurpose.com>
2018-05-08 18:14:38 -0400

    use WordPress jQuery (sucks, but plugins)

--- files/src/js/index.js
+++ files/src/js/index.js
@@ -2,10 +2,8 @@
 /* eslint no-unused-vars: ["error", {"vars": "local"}] */
 /* global __webpack_public_path__:true */
 
+const $ = window.jQuery;
+
 const path = require("path");
 __webpack_public_path__ =
   path.dirname(document.scripts[document.scripts.length - 1].src) + "/";
-
-const $ = require("jquery");
-window.$ = $;
-window.jQuery = $;
